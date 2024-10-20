9902d9326f6954914e9e39a587937b382dedbf72
Harsh Porwal <56501271+Harsh-Porwal424@users.noreply.github.com>
2024-10-20 22:48:41 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -6,3 +6,4 @@ export default {
       url: 'postgresql://ai-interview-mocker_owner:yNWru9JZHD8S@ep-weathered-dawn-a5h5wge5.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require',
     }
   };
+//
